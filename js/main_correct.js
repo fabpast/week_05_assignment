83ff70957222f6ee5b23f4332c29a06999c76cc9
fabpast <fab.past@hotmail.com>
2017-07-04 18:04:38 +1000

    5th_changes

--- js/main_correct.js
+++ js/main_correct.js
@@ -7,13 +7,8 @@ $(document).ready(function() {
       $('body').css('background-image', 'url(images/nyc.jpg)');
     } else if (lowerCase === "la" || lowerCase === "los angeles") {
       $('body').css('background-image', 'url(images/la.jpg)');
-      } else if (lowerCase === "austin") {
-        $('body').css('background-image', 'url(images/austin.jpg)');
-        } else if (lowerCase === "sydney") {
-          $('body').css('background-image', 'url(images/sydney.jpg)');
-        } else if (lowerCase === "san francisco" || lowerCase === "san fran" || lowerCase === "sanfran") {
-          $('body').css('background-image', 'url(images/sf.jpg)');
-          }
+    } else if (lowerCase === "austin") {
+      $('body').css('background-image', 'url(images/austin.jpg)');
   }
 // I moved these two variables (city and lowerCase) outside the if function because "else if" couldn't read them
   $('form').submit(doSomeStuff);
