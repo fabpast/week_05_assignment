1f529371d34f63b806a22108ee98406817f6ea68
fabpast <fab.past@hotmail.com>
2017-07-04 17:41:05 +1000

    4th_changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var doSomeStuff = function(event) {
     event.preventDefault();
-    if (lowerCase === "nyc" || lowerCase ==="new york city") {
+    if (lowerCase === "nyc" || lowerCase === "new york city") {
       $('body').css('background-image', 'url(images/nyc.jpg)');
     } else if (lowerCase === "la" || lowerCase === "los angeles") {
       $('body').css('background-image', 'url(images/la.jpg)');
@@ -10,5 +10,5 @@ $(document).ready(function() {
 // I moved these two variables (city and lowerCase) outside the if function because "else if" couldn't read them
   var city = $('#city-type').val();
   var lowerCase = city.toLowerCase();
-  $('form').submit(doSomeStuff)
+  $('form').submit(doSomeStuff);
 });
